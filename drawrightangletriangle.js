7884e4d4cccac0921529d7ada0d92542c4066cff
Rixongile Manyobe <lednah97@gmail.com>
2020-04-08 12:55:08 +0200

    exercise completed

---
+++ drawrightangletriangle.js
@@ -0,0 +1,9 @@
+function rightAngleTriangle(num) {
+    for (let i = 1; i < num + 1; i++) {
+        str = "#"
+        strRow = str.repeat(i)
+        console.log(strRow)
+
+    }
+}
+rightAngleTriangle(4)
